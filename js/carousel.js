acda215928370a8a4cbd5eec1663fa1d99434b6a
tamang <t.tamang@aozora-okinawa.com>
2023-12-19 18:20:27 +0900

    gallery download and event is created

--- js/carousel.js
+++ js/carousel.js
@@ -8,3 +8,106 @@ var swiper = new Swiper(".mySwiper", {
         clickable: true,
     },
 });
+
+
+
+var monthSwiper = new Swiper(".swiperMonth", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 3,
+    rewind: true,
+    grabCursor: true,
+    effect: "coverflow",
+    coverflowEffect: {
+        coverShadow: '0px 8px 16px rgba(0, 0, 0, 0.6)',
+        coverShadowOpacity: 0.7,
+    },
+});
+
+// initial slide
+var currentDate = new Date();
+var currentMonth = currentDate.getMonth() + 1;
+monthSwiper.slideTo((currentMonth - 1));
+
+// initial article list
+updateArticleList((currentMonth - 1));
+
+// console.log(monthSwiper.activeIndex);
+monthSwiper.on('slideChange', function () {
+    // console.log(monthSwiper.activeIndex);
+    updateArticleList(monthSwiper.activeIndex);
+});
+
+monthSwiper.on('tap', function (event) {
+    // console.log(event.clickedIndex);
+    var clickedIndex = event.clickedIndex;
+    monthSwiper.slideTo((clickedIndex - 1));
+});
+
+function updateArticleList(slideIndex) {
+    var articleList = document.getElementById('articles');
+
+    // Add article list content based on the selected slide
+    switch (slideIndex) {
+        case 0:
+            hideAllArticleLists();
+            document.getElementById('articles-list-0').style.display = 'block';
+            break;
+        case 1:
+            hideAllArticleLists();
+            document.getElementById('articles-list-1').style.display = 'block';
+            break;
+        case 2:
+            hideAllArticleLists();
+            document.getElementById('articles-list-2').style.display = 'block';
+            break;
+        case 3:
+            hideAllArticleLists();
+            document.getElementById('articles-list-3').style.display = 'block';
+            break;
+        case 4:
+            hideAllArticleLists();
+            document.getElementById('articles-list-4').style.display = 'block';
+            break;
+        case 5:
+            hideAllArticleLists();
+            document.getElementById('articles-list-5').style.display = 'block';
+            break;
+        case 6:
+            hideAllArticleLists();
+            document.getElementById('articles-list-6').style.display = 'block';
+            break;
+        case 7:
+            hideAllArticleLists();
+            document.getElementById('articles-list-7').style.display = 'block';
+            break;
+        case 8:
+            hideAllArticleLists();
+            document.getElementById('articles-list-8').style.display = 'block';
+            break;
+        case 9:
+            hideAllArticleLists();
+            document.getElementById('articles-list-9').style.display = 'block';
+            break;
+        case 10:
+            hideAllArticleLists();
+            document.getElementById('articles-list-10').style.display = 'block';
+            break;
+        case 11:
+            hideAllArticleLists();
+            document.getElementById('articles-list-11').style.display = 'block';
+            break;
+        default:
+            articleList.innerHTML = '<p>No articles available</p>';
+    }
+}
+
+function hideAllArticleLists() {
+    var articleLists = document.querySelectorAll('.articles-list');
+    articleLists.forEach(function (list) {
+        list.style.display = 'none';
+    });
+}
+
--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,24 @@ button.addEventListener('click', () => {
 
 // carousel
 
+window.onscroll = function () {
+    // Get the navbar element
+    // var navbar = document.querySelector('nav');
+    var navbar = document.getElementById('navbar');
+    // var parentDiv = navbar.parentElement;
+    var navbarWidth=navbar.offsetWidth;
+    // Get the offset position of the navbar
+    var sticky = navbar.offsetTop;
+
+    // Add the sticky class to the navbar when you reach its scroll position
+    if (window.scrollY > 50) {
+        navbar.classList.add('sticky-nav');
+        navbar.style.width = navbarWidth + 'px';
+        document.getElementById('top-nav').style.display='none';
+        
+    } else {
+        navbar.classList.remove('sticky-nav');
+        navbar.style.width = 'auto';
+        document.getElementById('top-nav').style.display='block';
+    }
+};
